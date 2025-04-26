11fe895f6b0411aa8f8234d66a6e6f831e31a3b7
Shreya06-HP <sshreyaachari0610@gmail.com>
2025-04-26 12:10:06 +0530

    initaila commit

---
+++ src/main/java/selenium/practice/App.java
@@ -0,0 +1,16 @@
+package selenium.practice;
+import java.
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+    }
+}
